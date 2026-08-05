e8900b6573d16fab58de2dce958b12448bfe3cc2
Joel Rdz <joelrdzh@gmail.com>
2026-08-04 18:38:02 -0600

    js custom functions - parameters and arguments

--- playground/custom-functions/custom-functions.js
+++ playground/custom-functions/custom-functions.js
@@ -1,12 +1,36 @@
 // Function Definition
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
   // this is the function body
   console.log('Runnning Calculate Bill');
-  const total = 100 * 1.13;
+  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
   return total;
 }
 
 // Function Call or **Run**
-// console.log(`Your Total is ${calculateBill()}`);
-const myTotal = calculateBill();
-console.log(`Your Total is ${myTotal}`);
+// const amountValue = 500;
+// const taxRateValue = 0.3;
+// const myTotal = calculateBill(amountValue, taxRateValue);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+const greeting = sayHiTo('Wes');
+console.log(greeting); // Hello Wes
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name) {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+console.log(doctorize('wes')); // "Dr. wes"
+console.log(yell('wes')); // "HEY WES"
+
+// function as argument
+console.log(yell(doctorize('wes'))); // "HEY DR. WES"
+
+console.log(calculateBill(100));
+console.log(calculateBill(100, undefined, 0.2));
