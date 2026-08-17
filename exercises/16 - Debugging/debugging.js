a8dbbb5833ef82c0882b84931e251988072c93ca
Joel Rdz <joelrdzh@gmail.com>
2026-08-17 16:58:14 -0600

    Debugging: Breakpoints, Network Requests

--- exercises/16 - Debugging/debugging.js
+++ exercises/16 - Debugging/debugging.js
@@ -5,11 +5,8 @@ const people = [
 ];
 
 people.forEach((person, index) => {
-  // console.groupCollapsed(`${person.name}`);
-  // console.log(person.country);
-  // console.log(person.cool);
-  // console.log('DONE!');
-  // console.groupEnd(`${person.name}`);
+  debugger;
+  console.log(person.name);
 });
 
 // console.table(people);
@@ -78,3 +75,5 @@ async function fetchDadJoke() {
   console.log(joke);
   return joke;
 }
+
+// fetchDadJoke();
